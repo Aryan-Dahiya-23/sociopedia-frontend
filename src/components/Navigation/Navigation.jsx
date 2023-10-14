42d6d531585264026cd7a19cf370a4b7d4b67f9f
unknown <aryan23dahiya@gmail.com>
2023-10-14 17:32:25 +0530

    made some changes in navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -69,13 +69,13 @@ const Navigation = () => {
         // <div className="bg-white fixed w-full bottom-0 h-10 md:h-[360px] lg:h-auto lg:w-[15%] lg:bottom-auto lg:mt-[4%] lg:ml-[7.5%] lg:rounded-lg">
         // <div className="bg-white fixed w-full bottom-0 h-10 md:h-[360px] lg:relative lg:h-auto lg:w-[15%]  lg:mt-[5%] lg:ml-[7.5%] lg:rounded-lg">
 
-        // <div className="bg-white flex justify-center items-center fixed w-full h-14 px-5 border-t border-secondary-200 bottom-0 
-        // lg:absolute lg:h-[275px] lg:w-[15%] lg:p-0 lg:top-40 lg:left-28 lg:rounded-lg lg:border-none"
-        //     style={{ zIndex: 9999 }}>
-
-        <div className="bg-white flex justify-center items-center fixed w-full h-14 px-5 border-t border-secondary-200 bottom-0 md:h-[360px] 
-        lg:absolute lg:max-h-64 lg:w-[15%] lg:p-0 lg:top-40 lg:left-28 lg:rounded-lg lg:border-none"
+        <div className="bg-white flex justify-center items-center fixed w-full h-14 px-5 border-t border-secondary-200 bottom-0 
+        lg:absolute lg:h-[260px] lg:w-[15%] lg:p-0 lg:top-40 lg:left-28 lg:rounded-lg lg:border-none"
             style={{ zIndex: 9999 }}>
+
+            {/* <div className="bg-white flex justify-center items-center fixed w-full h-14 px-5 border-t border-secondary-200 bottom-0 md:h-[360px] 
+         lg:absolute lg:max-h-64 lg:w-[15%] lg:p-0 lg:top-40 lg:left-28 lg:rounded-lg lg:border-none"
+             style={{ zIndex: 9999 }}> */}
 
             <ul className="w-full flex flex-row justify-between items-center lg:justify-start lg:items-start lg:ml-4 lg:py-2 lg:flex-col">
 
